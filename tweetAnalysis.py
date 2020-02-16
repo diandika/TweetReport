93839f70c6ad85c909237806ea0943d61478d923
diandika <mohammaddicky.andika@gmail.com>
2020-02-16 11:17:46 +0700

    get tweets from specified users

--- tweetAnalysis.py
+++ tweetAnalysis.py
@@ -6,18 +6,24 @@ class TweetAnalysis:
     def __init__(self, username="diandika99"):
         file = open("api_key.json", "r")
         api_json = json.load(file)
-        self.consumer_key = api_json["CONSUMER_KEY"]
-        self.consumer_secret_key = api_json["CONSUMER_SECRET_KEY"]
-        self.acces_token = api_json["ACCESS_TOKEN"]
-        self.acces_secret_token = api_json["ACCESS_SECRET_TOKEN"]
+        self.twitter = twitter.Api(consumer_key=api_json["CONSUMER_KEY"],
+                                   consumer_secret=api_json["CONSUMER_SECRET_KEY"],
+                                   access_token_key=api_json["ACCESS_TOKEN"],
+                                   access_token_secret=api_json["ACCESS_SECRET_TOKEN"])
         self.username = username
 
     def connect(self):
-        twitter_connection = twitter.Api(consumer_key=self.consumer_key,
-                              consumer_secret=self.consumer_secret_key,
-                              access_token_key=self.acces_token,
-                              access_token_secret=self.acces_secret_token)
-        print(twitter_connection.VerifyCredentials())
+        print(self.twitter.VerifyCredentials())
+
+    def get_tweet(self, count=200, screen_name=None):
+        if screen_name is None:
+            username = self.username
+        else:
+            username = screen_name
+        print(count, username)
+        tweets = self.twitter.GetListTimeline(slug=username, owner_screen_name=username, count=count, return_json=True)
+        for tweet in tweets:
+            print("text:", tweet['text'])
 
     # def activity_frequency(self):
 

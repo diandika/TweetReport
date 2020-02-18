c52227ce3a1c58b8c8b65f2ea84beadda1ab5c12
diandika <mohammaddicky.andika@gmail.com>
2020-02-18 23:15:25 +0700

    find more tweets

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from tweetAnalysis import TweetAnalysis
 account = TweetAnalysis()
 
 # account.connect()
-account.get_tweet(count=200, screen_name='aimi_sound')
-account.get_liked_tweet()
+# account.get_tweet(count=200, screen_name='diandika99', page=5)
+account.get_liked_tweet(page=4)
 
 '''
 top = tkinter.Tk()
--- tweetAnalysis.py
+++ tweetAnalysis.py
@@ -3,6 +3,7 @@ import json
 
 import operator
 
+
 class TweetAnalysis:
     def __init__(self, username="diandika99"):
         file = open("api_key.json", "r")
@@ -16,41 +17,77 @@ class TweetAnalysis:
     def connect(self):
         print(self.twitter.VerifyCredentials())
 
-    def get_tweet(self, count=200, screen_name=None):
+    def get_tweet(self, count=200, screen_name=None, page=1):
         if screen_name is None:
             username = self.username
         else:
             username = screen_name
-        print(count, username)
-        tweets = self.twitter.GetListTimeline(slug=username, owner_screen_name=username, count=count, return_json=True)
-        count = 0
-        tweet_list = []
-        for tweet in tweets:
-            if "RT" in tweet["text"]:
-                tweet_list.append(tweet["text"])
-                count = count + 1
-        print("retweeted tweet count:", count)
+        if page > 16:
+            page = 16
 
-    def get_liked_tweet(self, count=200, screen_name=None):
+        max_id = None
+        all_tweets = []
+
+        for i in range(page):
+            print('iteration #', i+1)
+            tweets = self.twitter.GetListTimeline(slug=username, owner_screen_name=username, count=count,
+                                                  return_json=True, max_id=max_id)
+            latest_tweet = tweets[0]
+            if max_id is not None:
+                tweets.remove(latest_tweet)
+
+            if len(tweets) == 0:
+                print("search finished")
+                break
+
+            all_tweets.extend(tweets)
+            oldest_tweet = tweets[-1]
+            max_id = oldest_tweet["id"]
+            print("get:", len(all_tweets), "tweets")
+
+        if all_tweets is not None:
+            print("found", len(all_tweets), "tweets")
+
+    def get_liked_tweet(self, count=200, screen_name=None, page=1):
         if screen_name is None:
             username = self.username
         else:
             username = screen_name
-        print(count, username)
-        tweets = self.twitter.GetFavorites(screen_name=username, count=count, return_json=True)
+
+        max_id = None
+        all_tweets = []
         user_list = {}
-        for tweet in tweets:
-            screen_name = tweet["user"]["screen_name"]
-            if screen_name in user_list:
-                user_list[screen_name] += 1
-            else:
-                user_list[screen_name] = 1
-            # print(tweet["user"]["screen_name"])
+
+        for i in range(page):
+            print("iteration #", i+1)
+            tweets = self.twitter.GetFavorites(screen_name=username, count=count, return_json=True, max_id=max_id)
+            latest_tweet = tweets[0]
+            if max_id is not None:
+                tweets.remove(latest_tweet)
+
+            if len(tweets) == 0:
+                print("search finished")
+                break
+
+            oldest_tweet = tweets[-1]
+            max_id = oldest_tweet["id"]
+            all_tweets.extend(tweets)
+            print("get:", len(all_tweets), "tweets")
+            if len(tweets) == 0:
+                print("search finished")
+                break
+            for tweet in tweets:
+                screen_name = tweet["user"]["screen_name"]
+                if screen_name in user_list:
+                    user_list[screen_name] += 1
+                else:
+                    user_list[screen_name] = 1
+                # print(tweet["user"]["screen_name"])
 
         user_list = sorted(user_list.items(), key=operator.itemgetter(1), reverse=True)
 
         for user, count in user_list:
-            print(user, ':', count)
+            print("liked", user, "'s tweet:", count, "times")
 
     # def activity_frequency(self):
 
